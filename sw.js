c61936e3c14f307cccc98f6e09325afc2f3c0ae4
kevintrankt <kevin.tran.kt@gmail.com>
2017-05-07 01:17:59 -0700

    Finalized Caching

--- sw.js
+++ sw.js
@@ -4,6 +4,22 @@ self.addEventListener('install', function(e) {
       return cache.addAll([
         '/',
         'index.html',
+        'manifest.json',
+        'thinking.png',
+        'assets/js/main.js',
+        'assets/css/main.css',
+        'assets/js/util.js',
+        'assets/js/skel.min.js',
+        'assets/js/jquery.scrolly.min.js',
+        'assets/js/jquery.scrollzer.min.js',
+        'assets/js/jquery.min.js',
+        'images/avatar.jpg',
+        'images/banner.jpg',
+        'images/pic01.jpg',
+        'images/pic02.jpg',
+        'images/pic03.jpg',
+
+
       ]);
     })
   );
