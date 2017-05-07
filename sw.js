e8aebea053108fc2a3dec59fe257c47934832492
kevintrankt <kevin.tran.kt@gmail.com>
2017-05-07 01:04:00 -0700

    Test Each Root

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('the-magic-cache').then(function(cache) {
+      return cache.addAll([
+        '/'
+      ]);
     });
   );
 });
