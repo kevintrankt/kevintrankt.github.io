e1e3de5f3bfee7200b528bbd23074143ceda3a24
kevintrankt <kevin.tran.kt@gmail.com>
2017-05-07 00:55:32 -0700

    Fix service worker

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 	License: pixelarity.com/license
 */
 
-navigator.serviceWorker && navigator.serviceWorker.register('/sw.js').then(function(registration) {
+navigator.serviceWorker && navigator.serviceWorker.register('sw.js').then(function(registration) {
   console.log('Excellent, registered with scope: ', registration.scope);
 });
 
