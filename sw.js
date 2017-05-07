125574696d8465049e5117b4d3952b78c70f7669
kevintrankt <kevin.tran.kt@gmail.com>
2017-05-07 01:06:37 -0700

    Fix index.html

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('the-magic-cache').then(function(cache) {
       return cache.addAll([
-        '/'
+        '/',
         'index.html'
       ]);
     });
