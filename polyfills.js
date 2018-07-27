0f9cf6d33d6a9144e28a5e0eb157da58d4b2220b
Kevin Tran <kevin.tran.kt@gmail.com>
2018-07-27 00:51:25 -0700

    Revert

--- polyfills.js
+++ polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/hmmm/Documents/GitHub/csv-reader-2/csv/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/hmmm/Documents/GitHub/csv-reader/csv-reader/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
